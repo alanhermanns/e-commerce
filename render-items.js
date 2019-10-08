9db0fa40e59f20f581ecb411dac3f6942b22dd93
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-08 10:58:23 -0700

    working js, failing tests!

--- render-items.js
+++ render-items.js
@@ -1,6 +1,6 @@
 const renderProducts = (product) => {
     const li = document.createElement('li');
-    li.className = product.name;
+    li.className = product.category;
     li.title = product.description;
 
     const header3 = document.createElement('h3');
@@ -11,6 +11,21 @@ const renderProducts = (product) => {
     p.className = product.price;
     const USD = '$' + product.price.toFixed(2);
     p.textContent = USD;
+    li.appendChild(p);
+
+    const allTheDetails = document.createElement('details');
+    const detailsContent = document.createElement('p');
+    allTheDetails.textContent = product.description;
+    allTheDetails.appendChild(detailsContent);
+    li.appendChild(allTheDetails);
+
+    const addButton = document.createElement('button');
+    addButton.className = 'add';
+    addButton.textContent = 'Add to cart';
+    addButton.addEventListener('click', () => {
+        alert('It\'s in the cart!');
+    });
+    li.appendChild(addButton);
     
     return li;
 };
